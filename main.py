add50f6a953dbb25d757bd99dd3854f60b0b297b
Hegymegi - Jako Tamas G15 <hjtomipls@gmail.com>
2023-03-07 20:28:26 +0100

    updated req txt

--- main.py
+++ main.py
@@ -8,9 +8,10 @@ from flask_login import UserMixin, login_user, LoginManager, login_required, cur
 from forms import CreatePostForm, RegisterForm, LoginForm, CommentForm
 from flask_gravatar import Gravatar
 from functools import wraps
+import os
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = '8BYkEfBA6O6donzWlSihBXox7C0sKR6b'
+app.config['SECRET_KEY'] = os.environ.get("SECRET_KEY")
 ckeditor = CKEditor(app)
 Bootstrap5(app)
 login_manager = LoginManager(app)
@@ -28,7 +29,7 @@ app.jinja_env.globals["LOGGED_IN"] = False
 app.jinja_env.globals["IS_ADMIN"] = False
 
 # CONNECT TO DB
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///blog.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get("DATABSE_URL")
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
